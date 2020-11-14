748c02b651b0b218baeaf24a5069e7d0bf4fc860
jeffrosenthal <jeff@thecodingpit.com>
2020-11-14 11:46:45 -0500

    Initial commit

---
+++ UWP1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TwilioSmsUwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        private readonly MessageSender _messageSender;
+        public MainPage(MessageSender ms)
+        {
+            _messageSender = ms;
+            this.InitializeComponent();
+            CoreWindow.GetForCurrentThread().KeyDown += MainPage_KeyDown; ;
+        }
+
+        private void HandleAction()
+        {
+            _messageSender.SendSms(NumberInput.Text, MessageInput.Text);
+            Output.Text = $"Message sent to {NumberInput.Text} - {MessageInput.Text}";
+            MessageInput.Text = String.Empty;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HandleAction();
+        }
+        private void MainPage_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case Windows.System.VirtualKey.Enter:
+                    // handler for enter key
+                    HandleAction();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ UWP1/MessageSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace TwilioSmsUwp
+{
+    public class MessageSender
+    {
+        private readonly string _phonenumber;
+
+        public MessageSender()
+        {
+            var accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            var authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+            _phonenumber = Environment.GetEnvironmentVariable("TWILIO_PHONE_NUMBER");
+
+            TwilioClient.Init(accountSid, authToken);
+        }
+        public void SendSms(string toNumber, string body)
+        {
+            try
+            {
+                //Add the country code to the phone number if it is not there
+                if (!toNumber.StartsWith("+1"))
+                    toNumber = $"+1{toNumber}";
+
+                var message = MessageResource.Create(
+                    body: body,
+                    @from: new PhoneNumber(_phonenumber),
+                    to: new PhoneNumber(toNumber)
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
